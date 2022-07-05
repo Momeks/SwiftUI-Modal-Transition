67bcae0676ccfa6b368d0fb62cfe265ce34e5bc5
Mohammad Komeili <momeksdev@gmail.com>
2022-07-05 13:34:38 +0430

    Initial Commit

---
+++ SwiftUI Modal Transition/SwiftUI_Modal_TransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Modal_TransitionApp.swift
+//  SwiftUI Modal Transition
+//
+//  Created by Momeks on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Modal_TransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
